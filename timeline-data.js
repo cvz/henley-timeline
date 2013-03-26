7c760a0e07f1a2afde43a007ffa7e07badb05375
Stephen Vance <stephen.vance@cengage.com>
2013-03-26 16:23:52 -0400

    More data

--- timeline-data.js
+++ timeline-data.js
@@ -52,7 +52,7 @@ timelineData = [
     {
         "date": "1864/12/21",
         "title": "Destroying the Georgia Countryside",
-        "description": " A week after Lincoln’s re-election, Sherman set out across Georgia, taking the war to the Southern people themselves. Such a march would be “a demonstration to the world, foreign and domestic, that we have a power which Davis cannot resist,” Sherman argued. The triumphant army—sixty thousand strong and in five columns, spread across a broad swath of territory—made its way across the state in the fall of 1864. Sherman’s troops laid waste to the Georgia countryside. Union troops were ordered to destroy the railroad network and to forage for food, seizing it from nearby farmers. Troop commanders were empowered to destroy factories, cotton gins, and private homes in areas determined to be hostile, and were given instructions to seize horses, mules, and wagons as needed.  To make matters worse for residents in the path of Sherman’s soldiers, large numbers of deserters from both sides, fugitive slaves and outlaws all used the situation to inflict widespread destruction and panic. Sherman arrived at Savannah on December 21. “I beg to present to you, as a Christmas gift, the city of Savannah,” Sherman telegraphed Lincoln.",
+        "description": "A week after Lincoln’s re-election, Sherman set out across Georgia, taking the war to the Southern people themselves. Such a march would be “a demonstration to the world, foreign and domestic, that we have a power which Davis cannot resist,” Sherman argued. The triumphant army—sixty thousand strong and in five columns, spread across a broad swath of territory—made its way across the state in the fall of 1864. Sherman’s troops laid waste to the Georgia countryside. Union troops were ordered to destroy the railroad network and to forage for food, seizing it from nearby farmers. Troop commanders were empowered to destroy factories, cotton gins, and private homes in areas determined to be hostile, and were given instructions to seize horses, mules, and wagons as needed.  To make matters worse for residents in the path of Sherman’s soldiers, large numbers of deserters from both sides, fugitive slaves and outlaws all used the situation to inflict widespread destruction and panic. Sherman arrived at Savannah on December 21. “I beg to present to you, as a Christmas gift, the city of Savannah,” Sherman telegraphed Lincoln.",
         "location": "Savannah, GA",
         "thumbnail": "images/destroying the GA countryside.jpg"
     },
@@ -66,8 +66,29 @@ timelineData = [
     {
         "date": "1865/01/01",
         "title": " 4th U.S. Colored Infantry, Company E, at Fort Lincoln, ",
-        "description": " More than 180,000 African American men fought for the Union. Their military service helped convince policymakers to extend American citizenship to black men.",
+        "description": "More than 180,000 African American men fought for the Union. Their military service helped convince policymakers to extend American citizenship to black men.",
         "location": "Washington, D.C.",
         "thumbnail": "images/US_Colored_Infantry.jpg"
+    },
+    {
+        "date": "1865/04/14",
+        "title": "Lincoln Portrait before Assassination",
+        "description": "This picture of Lincoln, taken four days before his assassination, shows the toll four years of war had taken on the fifty-six-year-old president. The assassination of United States President Abraham Lincoln took place on April 14, 1865, as the American Civil War was drawing to a close. The assassination occurred five days after the commander of the Confederate Army of Northern Virginia, General Robert E. Lee, surrendered to Lieutenant General Ulysses S. Grant and the Union Army of the Potomac. Lincoln was the first American president to be assassinated, though an unsuccessful attempt had been made on Andrew Jackson 30 years before in 1835. The assassination was planned and carried out by the well-known stage actor John Wilkes Booth, as part of a larger conspiracy in a bid to revive the Confederate cause.",
+        "location": "Washington D.C.",
+        "thumbnail": "images/Abraham_Lincoln.jpg"
+    },
+    {
+        "date": "1865/01/01",
+        "title": "Union Party Campaign Poster",
+        "description": "With the war driving to a culmination, the Republicans called themselves the “Union Party,” nominated a Unionist from Tennessee alongside Lincoln, and painted pictures of peace and prosperity in their campaign poster",
+        "location": "Washington, D.C.",
+        "thumbnail": "images/UnionPartyCampaignPoster.jpg"
+    },
+    {
+        "date": "1864/01/01",
+        "title": "Clara Barton",
+        "description": "In 1864 she was appointed by Union General Benjamin Butler as the \"lady in charge\" of the hospitals at the front of the Army of the James. Among her more harrowing experiences was an incident in which a bullet tore through the sleeve of her dress without striking her and killed a man to whom she was tending. She is known as the \"Angel of the Battlefield.\"",
+        "location": "Washington, D.C.",
+        "thumbnail": "images/Barton,Clara.jpg"
     }
 ];
