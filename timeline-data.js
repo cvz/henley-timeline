b9ee6e44b7c8aef92e0978a112c3804678f0bf47
Stephen Vance <stephen.vance@cengage.com>
2013-03-26 17:11:32 -0400

    Minor data fixes

--- timeline-data.js
+++ timeline-data.js
@@ -2,42 +2,42 @@ timelineData = [
     {
         "date": "1861/07/21",
         "title": " First Battle of Bull Run ",
-        "description": " On July 21, Union troops flooded over the battlefield, fording the creeks, and flanking the strongest points in the Confederate lines. The Southern forces fell back. Confederate troop reinforcements soon poured out of a train at Manassas. The lack of a distinct uniform led some Northerners to mistake Southerners for their own troops. Soon realizing their mistakes, the Northerners began to pull back and run, giving the Confederacy its first victory.",
+        "description": "On July 21, Union troops flooded over the battlefield, fording the creeks, and flanking the strongest points in the Confederate lines. The Southern forces fell back. Confederate troop reinforcements soon poured out of a train at Manassas. The lack of a distinct uniform led some Northerners to mistake Southerners for their own troops. Soon realizing their mistakes, the Northerners began to pull back and run, giving the Confederacy its first victory.",
         "location": "Manassas, VA",
         "thumbnail": "images/battle-of-first-bull-run-l.jpg"
     },
     {
         "date": "1862/09/17",
         "title": " Antietam",
-        "description": " On September 17, Robert E. Lee and George McClellan battled at Antietam Creek, near Sharpsburg, Maryland. The Confederates had thirty-five thousand men to McClellan’s seventy-two thousand. The battle ended in confusion and stalemate. More men were killed, wounded, or declared missing on this day than on any other day in the Civil War.",
+        "description": "On September 17, Robert E. Lee and George McClellan battled at Antietam Creek, near Sharpsburg, Maryland. The Confederates had thirty-five thousand men to McClellan’s seventy-two thousand. The battle ended in confusion and stalemate. More men were killed, wounded, or declared missing on this day than on any other day in the Civil War.",
         "location": "Sharpsburg, VA",
         "thumbnail": "images/dead-soldier-antietam_small1.jpg"
     },
     {
         "date": "1862/11",
         "title": "Futile Assault on Richmond",
-        "description": " In November, Burnside planned for yet another ultimately futile assault on Richmond. He moved his troops to Fredericksburg, Virginia. With Southerners able to fire down on them from protected positions, Union soldiers were killed and injured in staggering numbers. At battle’s end, both armies remained where they had been at the beginning.",
+        "description": "In November, Burnside planned for yet another ultimately futile assault on Richmond. He moved his troops to Fredericksburg, Virginia. With Southerners able to fire down on them from protected positions, Union soldiers were killed and injured in staggering numbers. At battle’s end, both armies remained where they had been at the beginning.",
         "location": "Fredericksburg, VA",
         "thumbnail": "images/futile assault on richmond.jpg"
     },
     {
         "date": "1865/07/04",
         "title": "Defeat of Vicksburg",
-        "description": " On the Fourth of July, the very morning after the Union’s victory at Gettysburg, Vicksburg surrendered to Ulysses S. Grant. Vicksburg held tactical and psychological importance. It had been the symbol of Confederate doggedness and Union frustration. The Union’s conquest of Vicksburg in July and  Port Hudson in May—the last Confederate holdouts on the Mississippi River—meant that the river now divided the Confederacy while it tied the Union to the Gulf of Mexico. After hearing of the fall of Vicksburg, a pleased Lincoln exclaimed, “The father of waters again goes unvexed to the sea.",
+        "description": "On the Fourth of July, the very morning after the Union’s victory at Gettysburg, Vicksburg surrendered to Ulysses S. Grant. Vicksburg held tactical and psychological importance. It had been the symbol of Confederate doggedness and Union frustration. The Union’s conquest of Vicksburg in July and  Port Hudson in May—the last Confederate holdouts on the Mississippi River—meant that the river now divided the Confederacy while it tied the Union to the Gulf of Mexico. After hearing of the fall of Vicksburg, a pleased Lincoln exclaimed, “The father of waters again goes unvexed to the sea.",
         "location": "Vicksburg, VA",
         "thumbnail": "images/vicksburg_battle.JPG"
     },
     {
         "date": "1862/07",
         "title": "Troops Consolidated at Virginia Peninsula",
-        "description": " By July 1862, the war seemed to have reached a stalemate, prompting Lincoln to replace McClellan with more aggressive leadership. McClellan still commanded troops, but his role was restricted. The new Union leaders removed McClellan’s troops from the Virginia peninsula and consolidated them, under the joint command of General John Pope, with troops from the Shenandoah Valley. The forces began to move from the peninsula in August.",
+        "description": "By July 1862, the war seemed to have reached a stalemate, prompting Lincoln to replace McClellan with more aggressive leadership. McClellan still commanded troops, but his role was restricted. The new Union leaders removed McClellan’s troops from the Virginia peninsula and consolidated them, under the joint command of General John Pope, with troops from the Shenandoah Valley. The forces began to move from the peninsula in August.",
         "location": "Fredericksburg, VA",
         "thumbnail": "images/Union_soldiers_entrenched_along_the_west_bank_of_the_Rappahannock_River_at_Fredericksburg,_Virginia_(111-B-157).jpg"
     },
     {
         "date": "1863/11",
         "title": " Tennessee to Atlanta ",
-        "description": " After Vicksburg, Grant’s troops turned their attention to Tennessee. In November, they overpowered the Confederates on Lookout Mountain outside Chattanooga; two days later, Union soldiers shocked the Confederates by fighting their way up the steep Missionary Ridge. Chattanooga fell to Grant’s troops on November 23. The Union, now in control of the cities and rail junctions of Kentucky and Tennessee, had a direct route into Georgia.",
+        "description": "After Vicksburg, Grant’s troops turned their attention to Tennessee. In November, they overpowered the Confederates on Lookout Mountain outside Chattanooga; two days later, Union soldiers shocked the Confederates by fighting their way up the steep Missionary Ridge. Chattanooga fell to Grant’s troops on November 23. The Union, now in control of the cities and rail junctions of Kentucky and Tennessee, had a direct route into Georgia.",
         "location": "Chattanooga, TN",
         "thumbnail": "images/Battle of Lookout Mountain,Confederates on Lookout Mountain.jpg"
     },
@@ -45,7 +45,7 @@ timelineData = [
     {
         "date": "1864/09/02",
         "title": "Defeat of Atlanta",
-        "description": " While Grant would fight in Virginia, William T. Sherman would attack the railroad center of Atlanta, cutting off the Deep South from the Upper South. The loss of Atlanta would chop the Confederacy into pieces too small to resist the Northern army. Throughout June and July, Sherman pushed through Georgia toward Atlanta. By the end of July, the Southern army had fallen back into Atlanta, preparing to defend it. It seemed only a matter of time before the Union triumphed. Just when it appeared that the Democrats would unseat Lincoln, news from the battlefield changed everything: Sherman had begun destroying the railroads that made Atlanta an important junction. The Confederates set much of Atlanta on fire and abandoned it. Sherman and his army marched into the city on September 2.",
+        "description": "While Grant would fight in Virginia, William T. Sherman would attack the railroad center of Atlanta, cutting off the Deep South from the Upper South. The loss of Atlanta would chop the Confederacy into pieces too small to resist the Northern army. Throughout June and July, Sherman pushed through Georgia toward Atlanta. By the end of July, the Southern army had fallen back into Atlanta, preparing to defend it. It seemed only a matter of time before the Union triumphed. Just when it appeared that the Democrats would unseat Lincoln, news from the battlefield changed everything: Sherman had begun destroying the railroads that made Atlanta an important junction. The Confederates set much of Atlanta on fire and abandoned it. Sherman and his army marched into the city on September 2.",
         "location": "Atlanta, GA",
         "thumbnail": "images/Defeat of Atlanta.JPG"
     },
