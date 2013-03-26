45af58c05d3993ddf3ffde4d7958c9ac0e8fcbe1
Vitaliy Zakharov <kilativv@gmail.com>
2013-03-26 16:39:44 -0400

    Displaying timeline with months and years

--- timeline.js
+++ timeline.js
@@ -5,7 +5,7 @@
     render: function () {
       var template = Handlebars.compile($('#mainTemplate').html());
       $(this.el).html(template);
-      
+
       var tileCollection = new TileList();
       var tilesView = new TilesView({
         collection: tileCollection
@@ -48,18 +48,43 @@
   });
 
   var TimeLineView = Backbone.View.extend({
-    tagName: 'div',
+    tagName: 'canvas',
 
     initialize: function () {
-      _.bindAll(this,'render');
+      _.bindAll(this,'render', 'renderCanvas');
+      this.collection.on('reset', this.renderCanvas);
+
+    },
+
+    renderCanvas: function () {
+      this.$el.attr('width', '960px');
+      var context = this.el.getContext('2d');
+      context.strokeStyle = '#444';
+      context.moveTo(0, 20);
+      context.lineTo(960, 20);
+      context.stroke();
+
+      context.font = "bold 10px sans-serif";
+
+      var dates = this.collection.getDates();
+      var numOfMonths = _.last(dates).diff(_.first(dates), 'months');
+      var monthBlock = 960 / numOfMonths;
+      var curPos = 0;
+      var curDate = moment(_.last(dates));
+      while(numOfMonths--) {
+        if(curDate.month()===1) {
+          context.fillText(curDate.year(), curPos, 8);
+        }
+
+        context.moveTo(curPos, 10);
+        context.lineTo(curPos, 20);
+        curPos = curPos + monthBlock;  
+        curDate.subtract('months', 1);
+      }
+      context.stroke();
     },
 
     render: function () {
-      this.$el.append("<canvas id='timeLineCanvas'></canvas>");
-      var canvas = this.$el.find('#timeLineCanvas');
-      //var context = canvas.getContext('2d');
-      //context.fillStyle('#FF0000');
-      //context.fillRect('0,0,100,100');
       return this;  
     }
   });
@@ -69,9 +94,19 @@
   });
 
   var TileList = Backbone.Collection.extend({
-    model: Tile
+    model: Tile,
+
+    getDates: function () {
+      var datesSorted = this.pluck('date').map(function (date) {
+        return moment(date, 'YYYY/MM/DD');
+      }).sort(function (date) {
+        return date.unix();
+      });
+
+      return datesSorted;
+    },
   });
-  
+
   var mainView = new MainView();
   mainView.render();
   $(document).tooltip();
