8d80fd98f2a7bb974eab2b8bea64196a41be115f
Vitaliy Zakharov <kilativv@gmail.com>
2013-03-29 16:42:15 -0400

    commented out some data

--- timeline-data.js
+++ timeline-data.js
@@ -36,13 +36,13 @@ timelineData = [
          "location": "Fredericksburg, VA",
          "thumbnail": "images/Union_soldiers_entrenched_along_the_west_bank_of_the_Rappahannock_River_at_Fredericksburg,_Virginia_(111-B-157).jpg"
     },
-    {
-        "date": "1863/11",
-        "title": " Tennessee to Atlanta ",
-        "description": "After Vicksburg, Grant’s troops turned their attention to Tennessee. In November, they overpowered the Confederates on Lookout Mountain outside Chattanooga; two days later, Union soldiers shocked the Confederates by fighting their way up the steep Missionary Ridge. Chattanooga fell to Grant’s troops on November 23. The Union, now in control of the cities and rail junctions of Kentucky and Tennessee, had a direct route into Georgia.",
-        "location": "Chattanooga, TN",
-        "thumbnail": "images/Battle of Lookout Mountain,Confederates on Lookout Mountain.jpg"
-    },
+    //{
+    //    "date": "1863/11",
+    //    "title": " Tennessee to Atlanta ",
+    //    "description": "After Vicksburg, Grant’s troops turned their attention to Tennessee. In November, they overpowered the Confederates on Lookout Mountain outside Chattanooga; two days later, Union soldiers shocked the Confederates by fighting their way up the steep Missionary Ridge. Chattanooga fell to Grant’s troops on November 23. The Union, now in control of the cities and rail junctions of Kentucky and Tennessee, had a direct route into Georgia.",
+    //    "location": "Chattanooga, TN",
+    //    "thumbnail": "images/Battle of Lookout Mountain,Confederates on Lookout Mountain.jpg"
+    //},
 
     {
         "date": "1864/09/02",
@@ -66,7 +66,6 @@ timelineData = [
         "location": "Richmond, VA",
         "thumbnail": "images/RichmondBreadRiots.jpg"
     },
-<<<<<<< HEAD
     {
         "date": "1865/01/01",
         "title": "4th U.S. Colored Infantry, Company E, at Fort Lincoln, ",
@@ -96,34 +95,4 @@ timelineData = [
         "location": "Washington, D.C.",
         "thumbnail": "images/Barton,Clara.jpg"
     }
-=======
-   // {
-   //     "date": "1865/01/01",
-   //     "title": " 4th U.S. Colored Infantry, Company E, at Fort Lincoln, ",
-   //     "description": "More than 180,000 African American men fought for the Union. Their military service helped convince policymakers to extend American citizenship to black men.",
-   //     "location": "Washington, D.C.",
-   //     "thumbnail": "images/US_Colored_Infantry.jpg"
-   // },
-   // {
-   //     "date": "1865/04/14",
-   //     "title": "Lincoln Portrait before Assassination",
-   //     "description": "This picture of Lincoln, taken four days before his assassination, shows the toll four years of war had taken on the fifty-six-year-old president. The assassination of United States President Abraham Lincoln took place on April 14, 1865, as the American Civil War was drawing to a close. The assassination occurred five days after the commander of the Confederate Army of Northern Virginia, General Robert E. Lee, surrendered to Lieutenant General Ulysses S. Grant and the Union Army of the Potomac. Lincoln was the first American president to be assassinated, though an unsuccessful attempt had been made on Andrew Jackson 30 years before in 1835. The assassination was planned and carried out by the well-known stage actor John Wilkes Booth, as part of a larger conspiracy in a bid to revive the Confederate cause.",
-   //     "location": "Washington D.C.",
-   //     "thumbnail": "images/Abraham_Lincoln.jpg"
-   // },
-   // {
-   //     "date": "1865/01/01",
-   //     "title": "Union Party Campaign Poster",
-   //     "description": "With the war driving to a culmination, the Republicans called themselves the “Union Party,” nominated a Unionist from Tennessee alongside Lincoln, and painted pictures of peace and prosperity in their campaign poster",
-   //     "location": "Washington, D.C.",
-   //     "thumbnail": "images/UnionPartyCampaignPoster.jpg"
-   // },
-   // {
-   //     "date": "1864/01/01",
-   //     "title": "Clara Barton",
-   //     "description": "In 1864 she was appointed by Union General Benjamin Butler as the \"lady in charge\" of the hospitals at the front of the Army of the James. Among her more harrowing experiences was an incident in which a bullet tore through the sleeve of her dress without striking her and killed a man to whom she was tending. She is known as the \"Angel of the Battlefield.\"",
-   //     "location": "Washington, D.C.",
-   //     "thumbnail": "images/Barton,Clara.jpg"
-   // }
->>>>>>> timeline with all the data
-];
+]
