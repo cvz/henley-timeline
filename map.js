0f0cb6863b642a7c03bfd2516542424ba9652768
Stephen Vance <stephen.vance@cengage.com>
2013-03-29 14:12:36 -0400

    Prevent map zoom and clean up some map presentation

--- map.js
+++ map.js
@@ -1,13 +1,14 @@
 (function($){
-console.log('map: '+JSON.stringify(map));
-$.fn.vectorMap('addMap', 'us-map', map);
-$('#map').vectorMap(
-{map: 'us-map',
-backgroundColor: 'gray',
- focusOn: {
-  x: 1,
-  y: 0.5,
-  scale: 2
- }
-});
+    console.log('map: '+JSON.stringify(map));
+    $.fn.vectorMap('addMap', 'us-map', map);
+    $('#map').vectorMap({
+        map: 'us-map',
+        backgroundColor: 'gray',
+        zoomOnScroll: false,
+        focusOn: {
+            x: 1,
+            y: 0.5,
+            scale: 2
+        }
+    });
 })(jQuery);
